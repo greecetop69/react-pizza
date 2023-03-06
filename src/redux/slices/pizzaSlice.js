15f32bc3e80791316a55f3ec0888b418f5c5f151
acmem <acme.ms12@mai.ru>
2023-03-06 14:44:50 +0200

    asyncThunk rtk (dont work)

--- src/redux/slices/pizzaSlice.js
+++ src/redux/slices/pizzaSlice.js
@@ -1,4 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+export const fetchPizzas = createAsyncThunk(
+    'pizza/fetchPizzasStatus',
+    async (params) => {
+        const { category,
+            sortBy,
+            order,
+            search,
+            currentPage,
+            searchValue
+        } = params
+        const { data } = await axios.get(
+            `https://639262efac688bbe4c62c42b.mockapi.io/items/?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&search=${searchValue}`,
+        );
+        return data
+    }
+)
 
 const initialState = {
     items: []
@@ -9,6 +27,17 @@ const pizzaSlice = createSlice({
     reducers: {
         setItems(state, action) {
             state.items = action.payload
+        }
+    },
+    extraReducers: {
+        [fetchPizzas.pending]: (state, action) => {
+            console.log((state, "идет отправка"))
+        },
+        [fetchPizzas.fulfilled]: (state, action) => {
+            console.log((state, "отправленно"))
+        },
+        [fetchPizzas.rejected]: (state, action) => {
+            console.log(("ошибка отправки"))
         }
     }
 })
